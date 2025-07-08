c4c90111f6244100e4cfe693ec2884caa86943f4
thadeu-alves <thadeutelecentrosg@gmail.com>
2025-07-08 11:32:56 -0300

    update home page design

--- app/componets/Profile.tsx
+++ app/componets/Profile.tsx
@@ -4,27 +4,30 @@ import { ButtonLink } from "./ui/ButtonLink";
 export function Profile() {
     return (
         <div className="py-6 px-4 space-y-8 border-b-2 border-[var(--primary)] container mx-auto">
-            <div className="flex gap-4 items-center px-6">
+            <div className="flex items-center px-6 gap-8 justify-center">
                 <Image
                     alt="back icon"
                     src="/food.png"
                     width={100}
                     height={100}
-                    className="object-cover w-32 h-32 rounded-full border-2 border-[var(--primary)]"
+                    className="object-cover w-50 h-50 rounded-full border-2 border-[var(--primary)]"
                 />
 
-                <div className="space-y-4">
+                <div className="space-y-4 w-60">
                     <h1 className="font-semibold text-3xl">
                         Restaurant
                     </h1>
                     <p className="text-[var(--primary)]">
                         Your special restaurant
                     </p>
+                    <ButtonLink
+                        color="bg-primary"
+                        to="/cardapio"
+                    >
+                        Special Menu
+                    </ButtonLink>
                 </div>
             </div>
-            <ButtonLink color="bg-primary" to="/cardapio">
-                Special Menu
-            </ButtonLink>
         </div>
     );
 }
--- app/componets/ui/ButtonLink.tsx
+++ app/componets/ui/ButtonLink.tsx
@@ -12,7 +12,7 @@ export function ButtonLink({
     return (
         <Link href={to || "/"}>
             <div
-                className={`${color} w-1/2 text-center py-2 rounded text-white mx-auto `}
+                className={`${color} w-full text-center py-2 rounded text-white mx-auto `}
             >
                 <h1 className="font-semibold ">
                     {children}
