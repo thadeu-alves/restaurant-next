1969dda8e5c3068dc33f74c19b287d16648fb21c
thadeu-alves <thadeutelecentrosg@gmail.com>
2025-09-09 11:41:44 -0300

    responsivity

---
+++ app/components/PartnerSection.tsx
@@ -0,0 +1,20 @@
+export function PartnerSection() {
+    return (
+        <div className="md:px-12">
+            <div className="bg-amber-400 py-8 px-12">
+                <div>
+                    <h1>
+                        We Always Provide You The Best In
+                        Town
+                    </h1>
+                    <p>
+                        Lorem Ipsum is simply dummy text of
+                        the printing and typesetting
+                        industry.
+                    </p>
+                </div>
+                <div className="w-full h-full bg-amber-600"></div>
+            </div>
+        </div>
+    );
+}
