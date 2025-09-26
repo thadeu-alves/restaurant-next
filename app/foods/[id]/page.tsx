82aee8626ea458ddb052b19ee771a90a0261c14e
thadeu-alves <thadeutelecentrosg@gmail.com>
2025-09-25 21:05:30 -0300

    implements food view page

--- app/foods/[id]/page.tsx
+++ app/foods/[id]/page.tsx
@@ -1,10 +1,11 @@
 "use client";
+import { FoodList } from "@/app/components/ui/FoodList";
 import { FoodView } from "@/app/components/ui/FoodView";
 import { Header } from "@/app/components/ui/Header";
 import { Loading } from "@/app/components/ui/Loading";
 import { Section } from "@/app/components/ui/Section";
 import { connection } from "@/lib/connection";
-import { IFood } from "@/types";
+import { Category, IFood } from "@/types";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -14,6 +15,8 @@ export default function Page() {
 
     const [food, setFood] = useState<IFood>();
     const [loading, setLoading] = useState(false);
+    const [recommended, setRecommended] =
+        useState<Category>();
 
     const [error, setError] = useState<string | null>(null);
 
@@ -39,16 +42,56 @@ export default function Page() {
             }
         }
         fetchData();
-    }, [id]);
+    }, [id, food?.categoryId]);
+
+    useEffect(() => {
+        async function fetchData() {
+            setLoading(true);
+            try {
+                if (!food?.categoryId) return;
+
+                const res = await connection.get(
+                    `/categories/${food?.categoryId}`
+                );
+
+                if (!res.ok) {
+                    setError(
+                        "Erro ao procurar comidas recomendadas."
+                    );
+                }
+
+                const { data } = await res.json();
+
+                setRecommended(data);
+            } catch (err) {
+                console.log(err);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchData();
+    }, [food?.categoryId]);
 
     return (
         <Section.Container>
             <Header />
 
-            <div>
+            <div className="space-y-16">
                 <Loading loading={loading} />
 
                 {food && <FoodView {...food} />}
+
+                {recommended && (
+                    <div className="space-y-4">
+                        <h1 className="text-primary text-lg font-bold uppercase">
+                            Recommended
+                        </h1>
+                        <FoodList
+                            foods={recommended.foods || []}
+                            loading={loading}
+                        />
+                    </div>
+                )}
 
                 {error && (
                     <h1 className="font-semibold text-xl text-center">
